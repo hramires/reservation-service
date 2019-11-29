22e8bc623e58b8fe5e22b60c8d5a6c95d4fb0f7c
Jonathan Souza <jsouzajava@gmail.com>
2019-11-28 23:31:35 -0300

    Adjust

--- src/main/java/com/reservation/controller/reservation/request/ReservationCheckoutRequest.java
+++ src/main/java/com/reservation/controller/reservation/request/ReservationCheckoutRequest.java
@@ -6,4 +6,37 @@ public class ReservationCheckoutRequest {
     private String checkin;
     private String checkout;
 
+    public ReservationCheckoutRequest() {
+    }
+
+    public ReservationCheckoutRequest(String cpf, String checkin, String checkout) {
+        this.cpf = cpf;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
 }
