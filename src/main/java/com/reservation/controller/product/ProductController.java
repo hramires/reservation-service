79950910f6d2720278a226a4b11cb28944784be2
Jonathan Souza <jsouzajava@gmail.com>
2019-11-28 21:49:28 -0300

    Adjust @CrossOrigin

--- src/main/java/com/reservation/controller/product/ProductController.java
+++ src/main/java/com/reservation/controller/product/ProductController.java
@@ -4,10 +4,12 @@ import com.reservation.controller.product.request.ProductRequest;
 import com.reservation.controller.product.response.ProductResponse;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public interface ProductController {
 
+    @CrossOrigin
     @PostMapping("")
     @ApiOperation(value = "API used to insert a list of products that user consumes", response = ProductResponse.class)
     public ResponseEntity<ProductResponse> postProducts(ProductRequest productRequest);
--- src/main/java/com/reservation/controller/user/UserController.java
+++ src/main/java/com/reservation/controller/user/UserController.java
@@ -1,17 +1,19 @@
 package com.reservation.controller.user;
 
-import com.reservation.controller.product.response.ProductResponse;
 import com.reservation.controller.user.response.CostumerResponse;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public interface UserController {
 
+    @CrossOrigin
     @GetMapping("")
     @ApiOperation(value = "API used to get a list of costumers", response = CostumerResponse.class)
     public ResponseEntity<CostumerResponse> getCostumers();
 
+    @CrossOrigin
     @GetMapping("/integrate")
     @ApiOperation(value = "API used to integrate with amazon s3 and read a file inside s3", response = Boolean.class)
     public ResponseEntity<Boolean> executeIntegration();
