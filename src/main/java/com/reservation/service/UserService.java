e6cc76bb3586eb35796011cae75146cd2551c68f
Jonathan Souza <jsouzajava@gmail.com>
2019-11-29 18:17:16 -0300

    Adding some logs

--- src/main/java/com/reservation/service/UserService.java
+++ src/main/java/com/reservation/service/UserService.java
@@ -26,6 +26,7 @@ public class UserService {
 
     public void save(List<UserDto> usersDto) {
         List<User> users = userConverter.convertFromDto(usersDto);
+        users.stream().forEach(System.out::println);
         users.stream().map(user -> userRepository.save(user));
     }
 
